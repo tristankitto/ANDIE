c69e0861aa8d636f39b613489f3f8e384a483418
yima2520 <yima2520@student.otago.ac.nz>
2023-03-15 15:43:28 +1300

    Started work on Median Filter

---
+++ src/cosc202/andie/MedianFilter.java
@@ -0,0 +1,114 @@
+package cosc202.andie;
+
+import java.awt.image.*;
+import java.util.*;
+
+/**
+ * <p>
+ * ImageOperation to apply a Median (simple blur) filter.
+ * </p>
+ * 
+ * <p>
+ * A Median filter blurs an image by replacing each pixel by the median of the
+ * pixels in a surrounding neighbourhood, and can be implemented by a _____?.
+ * </p>
+ * 
+ * <p> 
+ * <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a>
+ * </p>
+ * 
+ * @see java.awt.image.ConvolveOp
+ * @author Steven Mills
+ * @version 1.0
+ */
+public class MedianFilter implements ImageOperation, java.io.Serializable {
+    
+    /**
+     * The size of filter to apply. A radius of 1 is a 3x3 filter, a radius of 2 a 5x5 filter, and so forth.
+     */
+    private int radius;
+
+    /**
+     * <p>
+     * Construct a Median filter with the given size.
+     * </p>
+     * 
+     * <p>
+     * The size of the filter is the 'radius' of the area considered.
+     * A size of 1 is a 3x3 filter, 2 is 5x5, and so on.
+     * Larger filters give a stronger blurring effect.
+     * </p>
+     * 
+     * @param radius The radius of the newly constructed MedianFilter
+     */
+    MedianFilter(int radius) {
+        this.radius = radius;    
+    }
+
+    /**
+     * <p>
+     * Construct a Median filter with the default size.
+     * </p
+     * >
+     * <p>
+     * By default, a Median filter has radius 1.
+     * </p>
+     * 
+     * @see MedianFilter(int)
+     */
+    MedianFilter() {
+        this(1);
+    }
+
+    /**
+     * <p>
+     * Apply a Median filter to an image.
+     * </p>
+     * 
+     * <p>
+     * As with many filters, the Median filter is implemented via convolution.
+     * The size of the convolution kernel is specified by the {@link radius}.  
+     * Larger radii lead to stronger blurring.
+     * </p>
+     * 
+     * @param input The image to apply the Median filter to.
+     * @return The resulting (blurred)) image.
+     */
+    public BufferedImage apply(BufferedImage input) {
+        
+        
+        for (int y = 0; y < input.getHeight(); ++y) {
+            if (y > radius && y < input.getHeight()) {
+                for (int x = 0; x < input.getWidth(); ++x) {
+                    if (x > radius && x < input.getHeight() - radius){
+                        int argb = input.getRGB(x, y);
+                        int a = (argb & 0xFF000000) >> 24;
+                        int r = (argb & 0x00FF0000) >> 16;
+                        int g = (argb & 0x0000FF00) >> 8;
+                        int b = (argb & 0x000000FF);
+        
+        
+        
+                        //int grey = (int) Math.round(0.3*r + 0.6*g + 0.1*b);
+        
+                        //argb = (a << 24) | (grey << 16) | (grey << 8) | grey;
+                        //input.setRGB(x, y, argb);
+                    }
+                }
+            }
+        }
+        
+        int size = (2*radius+1) * (2*radius+1);
+        float [] array = new float[size];
+        Arrays.fill(array, 1.0f/size);
+
+        Kernel kernel = new Kernel(2*radius+1, 2*radius+1, array);
+        ConvolveOp convOp = new ConvolveOp(kernel);
+        BufferedImage output = new BufferedImage(input.getColorModel(), input.copyData(null), input.isAlphaPremultiplied(), null);
+        convOp.filter(input, output);
+
+        return output;
+    }
+
+
+}
