6cfdb6deea04d05a85029ee11e45db1ade721e3f
Tristan Kitto <kittr801@student.otago.ac.nz>
2023-03-21 11:35:31 +1300

    Corrected comments for LanguageActions

--- src/cosc202/andie/LanguageActions.java
+++ src/cosc202/andie/LanguageActions.java
@@ -4,33 +4,13 @@ import java.util.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-/**
- * <p>
- * Actions provided by the File menu.
- * </p>
- * 
- * <p>
- * The File menu is very common across applications,
- * and there are several items that the user will expect to find here.
- * Opening and saving files is an obvious one, but also exiting the program.
- * </p>
- * 
- * <p>
- * <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA
- * 4.0</a>
- * </p>
- * 
- * @author Steven Mills
- * @version 1.0
- */
 public class LanguageActions {
 
-    /** A list of actions for the File menu. */
     protected ArrayList<Action> actions;
 
     /**
      * <p>
-     * Create a set of File menu actions.
+     * Create a set of Language menu actions.
      * </p>
      */
     public LanguageActions() {
@@ -38,20 +18,20 @@ public class LanguageActions {
         actions.add(new LanguageChoice(ResourceBundle.getBundle("cosc202.andie.LanguageBundle").getString("english"),
                 null, ResourceBundle.getBundle("cosc202.andie.LanguageBundle").getString("englishlanguage"),
                 Integer.valueOf(KeyEvent.VK_E), "en"));
-        actions.add(new LanguageChoice(ResourceBundle.getBundle("cosc202.andie.LanguageBundle").getString("spanish"),
-                null, ResourceBundle.getBundle("cosc202.andie.LanguageBundle").getString("spanishlanguage"),
-                Integer.valueOf(KeyEvent.VK_S), "es"));
         actions.add(new LanguageChoice(ResourceBundle.getBundle("cosc202.andie.LanguageBundle").getString("maori"),
                 null, ResourceBundle.getBundle("cosc202.andie.LanguageBundle").getString("maorilanguage"),
                 Integer.valueOf(KeyEvent.VK_M), "mi"));
+        actions.add(new LanguageChoice(ResourceBundle.getBundle("cosc202.andie.LanguageBundle").getString("spanish"),
+                null, ResourceBundle.getBundle("cosc202.andie.LanguageBundle").getString("spanishlanguage"),
+                Integer.valueOf(KeyEvent.VK_S), "es"));
     }
 
     /**
      * <p>
-     * Create a menu contianing the list of File actions.
+     * Create a menu contianing the list of Language actions.
      * </p>
      * 
-     * @return The File menu UI element.
+     * @return The Language menu UI element.
      */
     public JMenu createMenu() {
         JMenu fileMenu = new JMenu(ResourceBundle.getBundle("cosc202.andie.LanguageBundle").getString("language"));
@@ -65,7 +45,7 @@ public class LanguageActions {
 
     /**
      * <p>
-     * Action to open an image from file.
+     * Action to choose a language.
      * </p>
      * 
      * @see EditableImage#open(String)
@@ -75,13 +55,14 @@ public class LanguageActions {
 
         /**
          * <p>
-         * Create a new file-open action.
+         * Create a new language choice action.
          * </p>
          * 
          * @param name     The name of the action (ignored if null).
          * @param icon     An icon to use to represent the action (ignored if null).
          * @param desc     A brief description of the action (ignored if null).
          * @param mnemonic A mnemonic key to use as a shortcut (ignored if null).
+         * @param code     A 2 letter code used to represent the language selected.
          */
         LanguageChoice(String name, ImageIcon icon, String desc, Integer mnemonic, String code) {
             super(name, icon, desc, mnemonic);
@@ -90,12 +71,12 @@ public class LanguageActions {
 
         /**
          * <p>
-         * Callback for when the file-open action is triggered.
+         * Callback for when the language choice action is triggered.
          * </p>
          * 
          * <p>
-         * This method is called whenever the FileOpenAction is triggered.
-         * It prompts the user to select a file and opens it as an image.
+         * This method is called whenever the LanguageChoice is triggered.
+         * It changes the language of the GUI.
          * </p>
          * 
          * @param e The event triggering this callback.
