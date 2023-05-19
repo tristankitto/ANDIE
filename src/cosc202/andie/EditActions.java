e075c88077190796c0862a95cf9e914bcce28cc2
yima2520 <yima2520@student.otago.ac.nz>
2023-05-19 14:15:23 +1200

    Add text editing (incomplete)

--- src/cosc202/andie/EditActions.java
+++ src/cosc202/andie/EditActions.java
@@ -4,6 +4,12 @@ import java.util.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 /**
  * <p>
  * Actions provided by the Edit menu.
@@ -43,6 +49,8 @@ public class EditActions {
                 Integer.valueOf(KeyEvent.VK_Z)));
         actions.add(new RedoAction(bundle.getString("redo"), null, bundle.getString("redo"),
                 Integer.valueOf(KeyEvent.VK_Y)));
+        actions.add(new TextAction(bundle.getString("text"), null, bundle.getString("text"),
+                Integer.valueOf(KeyEvent.VK_T)));
     }
 
     /**
@@ -144,6 +152,120 @@ public class EditActions {
             target.getImage().redo();
             target.repaint();
             target.getParent().revalidate();
+        }
+    }
+
+    /**
+     * <p>
+     * Action to add text.
+     * </p>
+     * 
+     * @see Text#apply()
+     */
+    public class TextAction extends ImageAction {
+
+        static int startX = target.getWidth()/2;
+        static int startY = target.getHeight()/2;
+        static int x = 0;
+        static int y = 0;
+        static int endX = target.getWidth();
+        static int endY = target.getHeight();
+        static boolean text = false;
+        static boolean isTexting = false;
+        EditableImage image = target.getImage();
+
+        /**
+         * <p>
+         * Create a new text action.
+         * </p>
+         * 
+         * @param name     The name of the action (ignored if null).
+         * @param icon     An icon to use to represent the action (ignored if null).
+         * @param desc     A brief description of the action (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut (ignored if null).
+         */
+        TextAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
+            super(name, icon, desc, mnemonic);
+        }
+
+        /**
+         * <p>
+         * Callback for when the text action is triggered.
+         * </p>
+         * 
+         * <p>
+         * This method is called whenever the TextAction is triggered.
+         * It adds a text box.
+         * </p>
+         * 
+         * @param e The event triggering this callback.
+         */
+        public void actionPerformed(ActionEvent e) {
+            if (isTexting || image.getCurrentImage() == null) {
+                return;
+            }
+            
+            isTexting = true;
+
+            startX = 0;
+            startY = 0;
+            endX = target.getWidth();
+            endY = target.getHeight();
+
+            target.setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR));
+            MouseListener mouseListener = new MouseAdapter() {
+                public void mousePressed(MouseEvent e) {
+                    text = true;
+                    startX = e.getX();
+                    startY = e.getY();
+                }
+
+                public void mouseReleased(MouseEvent e) {
+                    endX = e.getX();
+                    endY = e.getY();
+                    text = false;
+                    isTexting = false;
+
+                    // Perform the crop operation
+                    image.apply(new Text(startX, startY, endX, endY));
+                    target.setImage(image);
+                    target.repaint();
+                    target.getParent().revalidate();
+
+                    // Remove the mouse listeners after the crop is done
+                    target.removeMouseListener(this);
+                    target.setCursor(Cursor.getDefaultCursor());
+
+                }
+            };
+
+            target.addMouseListener(mouseListener);
+
+            MouseMotionListener mouseMotionListener = new MouseMotionAdapter() {
+                public void mouseDragged(MouseEvent e) {
+                    endX = e.getX();
+                    endY = e.getY();
+                    target.repaint();
+                }
+            };
+
+            target.addMouseMotionListener(mouseMotionListener);
+
+            Action keyAction = new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    target.setImage(image);
+                    target.repaint();
+                    target.getParent().revalidate();
+                    target.removeMouseListener(mouseListener);
+                    target.removeMouseMotionListener(mouseMotionListener);
+                    target.setCursor(Cursor.getDefaultCursor());
+                    return;
+                }
+            };
+            KeyStroke keyStroke = KeyStroke.getKeyStroke("ESCAPE");
+            Andie.imagePanel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(keyStroke, "keyAction");
+            Andie.imagePanel.getActionMap().put("keyAction", keyAction);
         }
     }
 
